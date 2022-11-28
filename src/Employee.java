e1328d70617447c72690961b97d44b4e3a328f53
Engel62 <sr.engel@bk.ru>
2022-11-28 15:55:17 +0300

    1- 7 done

--- src/Employee.java
+++ src/Employee.java
@@ -44,5 +44,4 @@ public class Employee {
     public int getId() {
         return id;
     }
-
 }
--- src/EmployeeAction.java
+++ src/EmployeeAction.java
@@ -1,4 +1,14 @@
 public class EmployeeAction {
+    public double sum(Employee[] employee) {
+        int sum = 0;
+        for (int i = 0; i < employee.length; i++) {
+            sum = sum + employee[i].getSalary();
+        }
+      return sum;
+    }
 
+    public void printSum(Employee[] employee) {
+        System.out.println("Сумму затрат на зарплаты " + sum(employee));
+    }
+    }
 
-}
