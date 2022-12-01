f256be67c1775cd004e2a500ff1c9198f9ce606d
Engel62 <sr.engel@bk.ru>
2022-12-01 13:56:40 +0300

    done

--- src/EmployeeAction.java
+++ src/EmployeeAction.java
@@ -16,32 +16,39 @@ public class EmployeeAction {
     }
     public static void maxSalary(Employee[] employee) {
         double maxSalary = 0;
+        int elementCount =0;
         for (Employee value : employee) {
             if (value.getSalary() > maxSalary) {
                 maxSalary = value.getSalary();
+                if (value != null) {
+                    elementCount ++;
+                }
             }
         }
         System.out.println("Максимальная зарплата " + maxSalary);
     }
     public static void minSalary(Employee[] employee) {
-        double minSalary = 100000;
-        for (Employee value : employee)
+        double minSalary = employee[0].getSalary();
+        int elementCount =0;
+        for (Employee value : employee) {
             if (value.getSalary() < minSalary) {
                 minSalary = value.getSalary();
+                if (value != null) {
+                    elementCount ++;
+                }
             }
+        }
         System.out.println("Минимальная зарплата  " + minSalary);
     }
     public static double averageSalary(Employee[] employee) {
-        double averageSalary;
         int elementCount =0;
-        for (int i = 0; i < employee.length; i++) {
-            if (employee[i] != null) {
-                elementCount ++;
+        for (Employee value : employee) {
+            if (value != null) {
+                elementCount++;
             }
         }
         return  EmployeeAction.sum(employee) / elementCount;
     }
-
     public static void printAverageSalary(Employee[] employee) {
         System.out.println("Cредняя зарплата " + averageSalary(employee));
     }
