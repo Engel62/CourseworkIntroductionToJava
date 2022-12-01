6fc8abd2d5933eeb055493b1f2e4b77850b383e7
Engel62 <sr.engel@bk.ru>
2022-12-01 19:29:30 +0300

    Исправленны недочеты

--- src/EmployeeAction.java
+++ src/EmployeeAction.java
@@ -1,40 +1,41 @@
 public class EmployeeAction {
     public static double sum(Employee[] employee) {
         int sum = 0;
-        for (Employee value : employee) {
+        for (Employee value : employee){
+        if (value != null){
             sum = sum + value.getSalary();
         }
-        return sum;
+    } return sum;
     }
+
     public static void printSum(Employee[] employee) {
         System.out.println("Сумму затрат на зарплаты " + sum(employee));
     }
+
     public static void printAllEmployees(Employee[] employee) {
         for (Employee value : employee) {
             System.out.println(value);
         }
     }
+
     public static void maxSalary(Employee[] employee) {
         double maxSalary = 0;
-        int elementCount =0;
-        for (Employee value : employee) {
-            if (value.getSalary() > maxSalary) {
-                maxSalary = value.getSalary();
-                if (value != null) {
-                    elementCount ++;
+        for (Employee value : employee)
+            if (value != null) {
+                if (value.getSalary() > maxSalary) {
+                    maxSalary = value.getSalary();
                 }
             }
-        }
+
         System.out.println("Максимальная зарплата " + maxSalary);
     }
     public static void minSalary(Employee[] employee) {
         double minSalary = employee[0].getSalary();
-        int elementCount =0;
         for (Employee value : employee) {
-            if (value.getSalary() < minSalary) {
-                minSalary = value.getSalary();
-                if (value != null) {
-                    elementCount ++;
+            if (value != null) {
+                if (value.getSalary() < minSalary) {
+                    minSalary = value.getSalary();
+
                 }
             }
         }
@@ -53,7 +54,8 @@ public class EmployeeAction {
         System.out.println("Cредняя зарплата " + averageSalary(employee));
     }
     public static void printNameSurnamePatronymic(Employee[]employee) {
-        for (Employee value : employee) {
+        for (Employee value : employee)
+            if (value != null){
             System.out.println(value.getSurname() + " " + value.getName() + " " + value.getPatronymic());
         }
 
